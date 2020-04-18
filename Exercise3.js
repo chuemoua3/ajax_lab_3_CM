9b08e21b387245b8a92b4a272a5b19279d45f1f6
Chue <chuemoua12@outlook.com>
2020-04-18 19:39:17 -0400

    first commit

---
+++ Exercise3.js
@@ -0,0 +1,20 @@
+/*Exercise3
+Date: 4/18/2020
+Write two functions to chain together. The first function, makeAllCaps, will take in an array of words and capitalize them. A second function, sortWords, will sort the words in alphabetical order. 
+If the array contains anything besides a single word (string), an error should be thrown.
+*/
+
+let array = ["nsx", "supra", "gtr", "rx7", "hello there", 3]
+
+function makeAllCaps(arrayCap){
+    return new Promise((resolve, reject) => {
+        let checkArray = arrayCap.filter(element) =>{
+            
+        }
+    })
+}
+
+function sortWords(arrayCap){
+    arrayCap.sort();
+    console.log(arrayCap);
+};
